f0d880ee1908eca9bc05f513b521c7642c325101
tanachock <tanachock.saelo@gmail.com>
2025-06-27 13:24:22 +0700

    update123

--- src/app/payment/payment.component.ts
+++ src/app/payment/payment.component.ts
@@ -90,7 +90,7 @@ export class PaymentComponent {
     try {
       this.isLoading = true;
       const result = await liff.openWindow({
-        url: 'https://900d-2001-fb1-29-1e53-dc47-eea2-5509-b6e8.ngrok-free.app',
+        url: 'https://www.youtube.com/watch?v=4Lmcadu8ghM',
         external: true,
       });
       console.log('Payment result:', result);
