1d411b5893b70fc6aa50a24efb96b2d068726838
tanachock <tanachock.saelo@gmail.com>
2025-06-27 12:57:47 +0700

    update

--- src/app/payment-success/payment-success.component.ts
+++ src/app/payment-success/payment-success.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import liff from '@line/liff';
 
 @Component({
@@ -6,11 +6,42 @@ import liff from '@line/liff';
   templateUrl: './payment-success.component.html',
   styleUrls: ['./payment-success.component.scss']
 })
-export class PaymentSuccessComponent implements OnInit {
+export class PaymentSuccessComponent implements OnInit, OnDestroy {
+  currentDate = new Date();
+  transactionId = this.generateTransactionId();
+  countdown = 5;
+  private countdownInterval: any;
 
   ngOnInit() {
-    setTimeout(() => {
+    this.startCountdown();
+  }
+
+  ngOnDestroy() {
+    if (this.countdownInterval) {
+      clearInterval(this.countdownInterval);
+    }
+  }
+
+  startCountdown() {
+    this.countdownInterval = setInterval(() => {
+      this.countdown--;
+      if (this.countdown <= 0) {
+        this.closeWindow();
+      }
+    }, 1000);
+  }
+
+  generateTransactionId(): string {
+    const timestamp = Date.now().toString();
+    const random = Math.random().toString(36).substring(2, 6).toUpperCase();
+    return `TXN${timestamp.slice(-6)}${random}`;
+  }
+
+  closeWindow() {
+    try {
       liff.closeWindow();
-    }, 5000);
+    } catch (error) {
+      console.log('LIFF closeWindow not available');
+    }
   }
 }
