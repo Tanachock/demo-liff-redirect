36e49985e255d53e49a49408c4c95f5aef42013c
tanachock <tanachock.saelo@gmail.com>
2025-06-27 13:55:55 +0700

    update

--- src/app/payment/payment.component.ts
+++ src/app/payment/payment.component.ts
@@ -87,9 +87,7 @@ export class PaymentComponent {
   }
 
   async pay() {
-    setTimeout(() => {
-      liff.closeWindow();
-    }, 1000);
+    liff.closeWindow();
     try {
       await liff.openWindow({
         url: 'https://900d-2001-fb1-29-1e53-dc47-eea2-5509-b6e8.ngrok-free.app?openExternalBrowser=1',
